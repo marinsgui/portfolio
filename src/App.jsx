01f16e1795d190da3376ece88a5e9bf7a4860da0
Guilherme Marins <106755788+marinsgui@users.noreply.github.com>
2022-11-06 10:37:42 -0300

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div className="App">
+      <header>
+        <nav>
+          <ul>
+            <li>
+              <a href="#">Sobre</a>
+            </li>
+            <li>
+              <a href="#">Projetos</a>
+            </li>
+            <li>
+              <a href="#">Contato</a>
+            </li>
+          </ul>
+        </nav>
+      </header>
+    </div>
+  )
+}
+
+export default App
