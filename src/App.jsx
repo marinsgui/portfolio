6c992ae0c0c317db6199cd882bfad2da77b557b2
Guilherme Marins <106755788+marinsgui@users.noreply.github.com>
2022-11-06 18:51:22 -0300

    Initial header

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
 import './App.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCode } from '@fortawesome/free-solid-svg-icons';
 
 function App() {
 
   return (
     <div className="App">
       <header>
+        <div className='logo'>
+          <a href="#">
+            <FontAwesomeIcon icon={faCode} className='code-logo' />
+          </a>
+        </div>
         <nav>
           <ul>
             <li>
@@ -19,6 +26,13 @@ function App() {
           </ul>
         </nav>
       </header>
+
+      <main>
+        <section>
+          <h1>Guilherme Marins</h1>
+          <p>Front-end Developer</p>
+        </section>
+      </main>
     </div>
   )
 }
