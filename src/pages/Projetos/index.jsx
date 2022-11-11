2008e9d433e8486801335c9dccaa669dc4c7186d
Guilherme Marins <106755788+marinsgui@users.noreply.github.com>
2022-11-11 20:28:01 -0300

    Primeiro projeto adicionado

--- src/pages/Projetos/index.jsx
+++ src/pages/Projetos/index.jsx
@@ -1,3 +1,16 @@
+import './styles.css';
+
 export default function Projetos() {
-    
+    return (
+        <main>
+            <section className='projects-container'>
+                <div className='item'>
+                    <a href="https://calculadora-de-imc-eta.vercel.app/" target='_blank'>
+                        <img src="src\assets\calculadora-de-imc.jpg" alt="Calculadora de IMC" />
+                    </a>
+                    <p>Calculadora de IMC</p>
+                </div>
+            </section>
+        </main>
+    )
 }
