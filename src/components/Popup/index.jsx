7da3c6e013412bad103979d6c19321dd78963887
Guilherme Marins <106755788+marinsgui@users.noreply.github.com>
2022-12-05 11:35:25 -0300

    alterações no popup

--- src/components/Popup/index.jsx
+++ src/components/Popup/index.jsx
@@ -1,10 +1,14 @@
 import './styles.css'
 
+import { FaWindowClose } from 'react-icons/fa';
+
 export default function Popup(props) {
     return (props.trigger) ? (
         <div className="popup-container">
             <div className="popup-content">
-                <button className="close-btn">close</button>
+                <button className="close-btn" onClick={() => props.setTrigger(false)}>
+                    <FaWindowClose />
+                </button>
                 {props.children}
             </div>
         </div>
--- src/pages/Projetos/index.jsx
+++ src/pages/Projetos/index.jsx
@@ -7,24 +7,30 @@ import weather_now_image from '../../assets/weather-now.jpg';
 import movies_lib_image from '../../assets/movies-lib.jpg';
 
 import Popup from '../../components/Popup';
+import { useState } from 'react';
 
 export default function Projetos() {
+
+    const [showPopup, setShowPopup] = useState(false);
+
     return (
         <main>
             <section className='projects-container'>
-                <div className='item'>
+                <div className='item' onClick={() => setShowPopup(true)}>
                         <img src={imc_image} alt="Calculadora de IMC" />
                     <p>Calculadora de IMC</p>
-                    <Popup trigger={true}>
+                    <Popup trigger={showPopup} setTrigger={setShowPopup}>
                     <a href="https://calculadora-de-imc-eta.vercel.app/" target='_blank'>Projeto</a>
                     <a href="https://github.com/marinsgui/calculadora-de-imc" target='_blank'>Repositório</a>
                     </Popup>
                 </div>
                 <div className='item'>
-                    <a href="https://movies-lib-blond.vercel.app/" target='_blank'>
                         <img src={movies_lib_image} alt="Movies Lib" />
-                    </a>
                     <p>Movies Lib</p>
+                    <Popup trigger={false}>
+                    <a href="https://movies-lib-blond.vercel.app/" target='_blank'>Projeto</a>
+                    <a href="https://github.com/marinsgui/movies-lib" target='_blank'>Repositório</a>
+                    </Popup>
                 </div>
                 <div className='item'>
                     <a href="https://explorer-lab-01-marinsgui.vercel.app/" target='_blank'>
