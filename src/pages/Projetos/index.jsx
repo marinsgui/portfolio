08fbed7cfccb11a541b19170505e6bc86dc4a7f9
Guilherme Marins <106755788+marinsgui@users.noreply.github.com>
2022-12-07 11:45:04 -0300

    adicionado links para projeto e repo

--- src/pages/Projetos/index.jsx
+++ src/pages/Projetos/index.jsx
@@ -6,49 +6,49 @@ import todo_list_image from '../../assets/todo-list.jpg';
 import weather_now_image from '../../assets/weather-now.jpg';
 import movies_lib_image from '../../assets/movies-lib.jpg';
 
-import Popup from '../../components/Popup';
-import { useState } from 'react';
-
 export default function Projetos() {
-
-    const [showPopup, setShowPopup] = useState(false);
-
     return (
         <main>
             <section className='projects-container'>
-                <div className='item' onClick={() => setShowPopup(true)}>
+                <div className='item'>
                         <img src={imc_image} alt="Calculadora de IMC" />
                     <p>Calculadora de IMC</p>
-                    <Popup trigger={showPopup} setTrigger={setShowPopup}>
-                    <a href="https://calculadora-de-imc-eta.vercel.app/" target='_blank'>Projeto</a>
-                    <a href="https://github.com/marinsgui/calculadora-de-imc" target='_blank'>Repositório</a>
-                    </Popup>
+                    <div className="links">
+                        <a href="https://calculadora-de-imc-eta.vercel.app/">Projeto</a>
+                        <a href="https://github.com/marinsgui/calculadora-de-imc">Repositório</a>
+                    </div>
                 </div>
                 <div className='item'>
                         <img src={movies_lib_image} alt="Movies Lib" />
                     <p>Movies Lib</p>
-                    <Popup trigger={false}>
-                    <a href="https://movies-lib-blond.vercel.app/" target='_blank'>Projeto</a>
-                    <a href="https://github.com/marinsgui/movies-lib" target='_blank'>Repositório</a>
-                    </Popup>
+                    <div className="links">
+                        <a href="https://movies-lib-blond.vercel.app/" target='_blank'>Projeto</a>
+                        <a href="https://github.com/marinsgui/movies-lib" target='_blank'>Repositório</a>
+                    </div>
                 </div>
                 <div className='item'>
-                    <a href="https://explorer-lab-01-marinsgui.vercel.app/" target='_blank'>
                         <img src={rocketpay_image} alt="Rocketpay" />
-                    </a>
                     <p>Rocketpay</p>
+                    <div className="links">
+                        <a href="https://explorer-lab-01-marinsgui.vercel.app/">Projeto</a>
+                        <a href="https://github.com/marinsgui/explorer-lab-01">Repositório</a>
+                    </div>
                 </div>
                 <div className='item'>
-                    <a href="https://todo-list-react-marinsgui.vercel.app/" target='_blank'>
                         <img src={todo_list_image} alt="Lista de tarefas" />
-                    </a>
                     <p>Lista de tarefas</p>
+                    <div className="links">
+                        <a href="https://todo-list-react-marinsgui.vercel.app/">Projeto</a>
+                        <a href="https://github.com/marinsgui/todo-list-react">Repositório</a>
+                    </div>
                 </div>
                 <div className='item'>
-                    <a href="https://weather-now-bay.vercel.app/" target='_blank'>
                         <img src={weather_now_image} alt="Weather Now" />
-                    </a>
                     <p>Weather Now</p>
+                    <div className="links">
+                        <a href="https://weather-now-bay.vercel.app/">Projeto</a>
+                        <a href="https://github.com/marinsgui/weather-now">Repositório</a>
+                    </div>
                 </div>
             </section>
         </main>
