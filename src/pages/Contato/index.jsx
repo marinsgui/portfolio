77b1fa687bb201ffe6b08e4ebfa6f416acbbbc1e
Guilherme Marins <106755788+marinsgui@users.noreply.github.com>
2022-12-08 10:31:25 -0300

    pagina de contato inicial

--- src/pages/Contato/index.jsx
+++ src/pages/Contato/index.jsx
@@ -1,7 +1,27 @@
+import './styles.css';
+
+import { FaLinkedin, FaGithub } from 'react-icons/fa';
+
 export default function Contato() {
-    <section>
-        <div>
-            <h2></h2>
-        </div>
-    </section>
+    return (
+        <section>
+            <div className='contacts'>
+                <h2>Entre em contato:</h2>
+                <ul>
+                    <li>
+                        <a href="https://www.linkedin.com/in/guilherme-marins/">
+                            <FaLinkedin />
+                            Linkedin
+                        </a>
+                    </li>
+                    <li>
+                        <a href="https://github.com/marinsgui">
+                            <FaGithub />
+                            Github
+                        </a>
+                    </li>
+                </ul>
+            </div>
+        </section>
+    )
 }
