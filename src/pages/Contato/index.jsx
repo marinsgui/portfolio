424e11a767a6036a619913471dc1480b32f4aaf4
Guilherme Marins <106755788+marinsgui@users.noreply.github.com>
2022-12-08 11:13:09 -0300

    formulario de email criado

--- src/pages/Contato/index.jsx
+++ src/pages/Contato/index.jsx
@@ -21,6 +21,16 @@ export default function Contato() {
                         </a>
                     </li>
                 </ul>
+                <h2>Envie um email:</h2>
+                <form action='https://formsubmit.co/marins0guilherme@gmail.com' method='POST'>
+                    <input type="text" name="name" id="name" placeholder='Insira seu nome' />
+                    <input type="email" name="email" id="email" placeholder='Insira seu email' />
+                    <textarea name="mensagem" id="mensagem" cols="50" rows="10" placeholder='Digite sua mensagem'></textarea>
+                    <button type="submit">Enviar mensagem</button>
+                    <input type="hidden" name="_subject" value='Novo Contato!' />
+                    <input type="text" name="_honey" style={{display: 'none'}} />
+                    <input type="hidden" name="_captcha" value='false' />
+                </form>
             </div>
         </section>
     )
