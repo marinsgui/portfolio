e5c0bc4fc441de2e32471c42474295622d230e3b
Guilherme Marins <106755788+marinsgui@users.noreply.github.com>
2022-12-08 11:40:34 -0300

    sobre inicial

--- src/pages/Sobre/index.jsx
+++ src/pages/Sobre/index.jsx
@@ -1,8 +1,13 @@
+import './styles.css';
+
 export default function Sobre() {
     return (
         <main>
             <section>
-                <h1>Olá</h1>
+                <h1>Sobre mim:</h1>                
+                <p>Olá, me chamo Guilherme Marins e sou um desenvolvedor web front-end.</p>
+                <p>Trabalho com tecnologias como HTML5, CSS3, JavaScript e React.</p>
+                <p>Estou em busca da primeira oportunidade, com o intuito de me desenvolver ainda mais em React e em outras tecnologias front-end.</p>
             </section>
         </main>
     )
