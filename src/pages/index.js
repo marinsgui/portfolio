9789c0012030e09977c84df5e1bdba32483a4a6d
Guilherme Marins <106755788+marinsgui@users.noreply.github.com>
2023-04-02 13:06:48 -0300

    header feito

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,10 @@
 import Head from 'next/head'
-import { Inter } from 'next/font/google'
-
-const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
     <>
       <Head>
-        <title>Create Next App</title>
+        <title>Guilherme Marins - Dev Front-End</title>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
       <main>
