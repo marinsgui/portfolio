8414dfc228c2b091140858319018c543bf24b992
Guilherme Marins <106755788+marinsgui@users.noreply.github.com>
2023-04-04 12:26:21 -0300

    Update About.jsx

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -6,13 +6,13 @@ export default function About() {
                     <p className="text-4xl font-bold inline border-b-4 border-gray-500">Sobre</p>
                 </div>
                 <p className="text-xl mt-20">
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui eaque aperiam illum autem facilis molestiae voluptatum omnis cum ea maxime, impedit veritatis sed provident beatae minus enim! Voluptatibus, repellendus eligendi.
+                Com formação em biomedicina e experiência em diagnósticos por imagem, decidi fazer uma mudança de carreira e me tornar um desenvolvedor front-end. Ao longo do caminho, desenvolvi habilidades em HTML, CSS, JavaScript e frameworks como React e NextJS. 
                 </p>
 
                 <br />
 
                 <p className="text-xl">
-                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Perspiciatis possimus consequatur repellat expedita, eius ea. Temporibus aliquam maiores totam porro distinctio esse, recusandae, rem vitae aliquid necessitatibus, voluptatibus provident omnis?
+                Meu conhecimento prévio na área da saúde me permitiu trazer uma perspectiva única para o desenvolvimento de produtos e interfaces de usuário intuitivas e acessíveis. Estou animado em continuar crescendo e aprendendo na área de desenvolvimento web.
                 </p>
             </div>
         </div>
