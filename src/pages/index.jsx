876099562f01995bdd2dbbd4fbba7169be469651
Guilherme Marins <106755788+marinsgui@users.noreply.github.com>
2023-04-04 10:28:50 -0300

    homepage componentizada

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,12 +1,9 @@
 import Head from 'next/head'
-import Image from 'next/image'
 
-import jsAnimated from '../assets/javascript-frameworks-animate.svg'
-
-import { MdOutlineArrowForwardIos } from 'react-icons/md' 
-
+// components
 import About from '@/components/About'
 import Projects from '@/components/Projects'
+import Homepage from '@/components/Homepage'
 
 export default function Home() {
   return (
@@ -14,29 +11,7 @@ export default function Home() {
       <Head>
         <title>Guilherme Marins - Dev Front-End</title>
       </Head>
-      <main name="home" className='h-screen bg-gradient-to-b from-black via-black to-gray-800 text-white md:pt-24'>
-        <div className='max-w-screen-lg mx-auto flex flex-col justify-center items-center px-4 md:flex-row gap-24'>
-          <div className='flex flex-col justify-center h-full max-w-md'>
-            <h2 className='text-4xl sm:text-7xl font-bold text-white'>Sou um desenvolvedor front-end</h2>
-            <p className='text-gray-500 py-4'>Desenvolvedor Front-End. Possuo conhecimento em HTML, CSS, JavaScript, React e outras tecnologias da web. Sou apaixonado por design e UX, e busco sempre criar soluções atraentes e intuitivas para o usuário.
-            Possuo forte capacidade de trabalho em equipe e sou capaz de colaborar com desenvolvedores de back-end para entregar projetos de alta qualidade no prazo estabelecido.
-            Estou sempre buscando me atualizar e aprender com as últimas tendências e tecnologias do mercado.</p>
-
-            <div>
-              <button className='group text-white w-fit px-6 py-3 my-2 flex items-center rounded-md bg-gradient-to-r from-cyan-500 to-blue-500'>
-                Projetos
-                <span className='group-hover:rotate-90 duration-300'>
-                  <MdOutlineArrowForwardIos size={20} className='pl-2' />
-                </span>
-              </button>
-            </div>
-          </div>
-
-          <div className='w-2/3 md:w-full'>
-            <Image src={jsAnimated} />
-          </div>
-        </div>
-      </main>
+      <Homepage />
       <About />
       <Projects />
     </>
