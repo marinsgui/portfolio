1b4d2d7ce0fafa06efcbfb89bb834f73f8e62f0a
marinsgui <marins0guilherme@gmail.com>
2023-08-07 15:31:30 -0300

    seção home alterada e botão para download de currículo adicionado

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -1,44 +1,42 @@
-import Image from 'next/image'
+import Image from "next/image";
 
-import jsAnimated from '../assets/javascript-frameworks-animate.svg'
+import doodlesImg from "../assets/doodles-mixed-round.svg";
+import manLaptopImg from "../assets/man-laptop.svg";
 
-import { ChevronRight } from 'lucide-react'
+import { ChevronRight, Download } from "lucide-react";
 
-import { Link } from 'react-scroll'
+import { Link } from "react-scroll";
 
 export default function Homepage() {
-    return (
-        <main name="Home" className='h-screen bg-gradient-to-b from-black via-black to-gray-800 text-white md:pt-24'>
-            <div className='max-w-screen-lg mx-auto flex flex-col justify-center items-center px-4 md:flex-row gap-24'>
-                <div className='flex flex-col justify-center h-full max-w-md'>
-                    <h2 className='text-4xl sm:text-7xl font-bold text-white'>
-                        Sou um desenvolvedor front-end
-                    </h2>
-                    <p className='text-gray-500 py-4'>
-                        Desenvolvedor Front-End. Possuo conhecimento em HTML, CSS, JavaScript, React e outras tecnologias da web. Sou apaixonado por design e UX, e busco sempre criar soluções atraentes e intuitivas para o usuário.
-                        Possuo forte capacidade de trabalho em equipe e sou capaz de colaborar com desenvolvedores de back-end para entregar projetos de alta qualidade no prazo estabelecido.
-                        Estou sempre buscando me atualizar e aprender com as últimas tendências e tecnologias do mercado.
-                    </p>
+  return (
+    <main name="Home" className="h-screen bg-sky-950 md:pt-24">
+      <div className="max-w-screen-lg mx-auto flex flex-col justify-center items-center px-4 md:flex-row">
+        <div className="flex flex-col justify-center h-full max-w-md">
+          <h2 className="text-4xl sm:text-7xl mt-20 font-bold text-white">
+            I&apos;M A <span className="text-teal-500">FRONT-END</span> DEVELOPER
+          </h2>
 
-                    <div>
-                        <Link
-                        to='Projetos'
-                        smooth
-                        duration={500} 
-                        className='group text-white w-fit px-6 py-3 my-2 flex items-center rounded-md bg-gradient-to-r from-cyan-500 to-blue-500 cursor-pointer'
-                        >
-                            Projetos
-                            <span className='group-hover:rotate-90 duration-300'>
-                                <ChevronRight size={20} className='pl-2' />
-                            </span>
-                        </Link>
-                    </div>
-                </div>
+          <div className="flex justify-start items-center gap-4">
+            <Link
+              to="Projetos"
+              smooth
+              duration={500}
+              className="group bg-teal-500 text-white w-fit px-4 py-2 my-10 flex items-center rounded-full cursor-pointer"
+            >
+              Projects
+              <span className="group-hover:rotate-90 duration-300">
+                <ChevronRight size={20} className="pl-2" />
+              </span>
+            </Link>
+            <a href="/Currículo.pdf" download="Currículo.pdf" className="flex justify-center items-center gap-2 w-fit bg-slate-600 text-white rounded-full px-4 py-2">Download CV <Download size={20} /></a>
+          </div>
+        </div>
 
-                <div className='w-2/3 md:w-full'>
-                    <Image src={jsAnimated} alt='' />
-                </div>
-            </div>
-        </main>
-    )
+        <div className="w-2/3 md:w-full relative">
+          <Image src={doodlesImg} width={500} alt="" className="absolute -top-28 right-0" />
+          <Image src={manLaptopImg} width={350} alt="" className="absolute top-0 right-28" />
+        </div>
+      </div>
+    </main>
+  );
 }
